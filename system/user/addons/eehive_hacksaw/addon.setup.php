7887fec390e319dcb9466c74ade651a6fcd98234
Chris Umphress <chris@manwithapeg.com>
2019-03-27 09:59:05 -0400

    Updated to work on EE5

---
+++ system/user/addons/eehive_hacksaw/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'author'      => 'EE Hive - Brett DeWoody',
+  'author_url'  => 'http://www.ee-hive.com/add-ons/hacksaw',
+  'name'        => 'EE Hive Hacksaw',
+  'description' => 'Allows you to create excerpts of your entries by removing HTML tags and limited the excerpt by character count, word count or a specific marker you insert into your content.',
+  'version'     => '1.08',
+  'namespace'   => 'Eehive\Hacksaw'
+);
